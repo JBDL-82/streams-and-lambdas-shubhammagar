598bcb3fdefc4435eaeaee815530163def179560
shubhammagar <magarshubham13@gmail.com>
2025-03-24 14:13:32 +0530

    2nd question

--- stream/src/App.java
+++ stream/src/App.java
@@ -1,18 +1,29 @@
-import java.lang.reflect.Array;
+import java.util.Map;q
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Employee employee1 = new Employee();
-        employee1.name = "John";
-        employee1.salary = 50001.0;
-        Employee employee2 = new Employee();
+        // Employee employee1 = new Employee();
+        // employee1.name = "John";
+        // employee1.salary = 50001.0;
+        // Employee employee2 = new Employee();
 
-        employee2.name = "Jane";
-        employee2.salary = 6000.0;
-        ArrayList<Employee> employees = new ArrayList<Employee>();
-        employees.add(employee1);
-        employees.add(employee2);
-        employees.stream().filter(e->e.salary>50000).forEach(e->System.out.println(e.name));
-    }
+        // employee2.name = "Jane";
+        // employee2.salary = 6000.0;
+        // ArrayList<Employee> employees = new ArrayList<Employee>();
+        // employees.add(employee1);
+        // employees.add(employee2);
+        // employees.stream().filter(e->e.salary>50000).forEach(e->System.out.println(e.name));
+
+        List<String> input = new ArrayList<String>();
+        input.add("Apple");
+        input.add("Banana");  
+        input.add("Apple");
+        
+        Map<String, Long> result = input.stream().collect(Collectors.groupingBy(word -> word,Collectors.counting()));
+        System.out.println(result);
+
+}
 }
